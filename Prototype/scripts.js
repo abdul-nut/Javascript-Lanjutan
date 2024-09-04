522a21020ecbb203ed453d108409760e7d3de1d4
abdul-nut <suryayasaantariksa@gmail.com>
2024-09-04 14:20:01 +0800

    menggunakan prototype

--- Prototype/scripts.js
+++ Prototype/scripts.js
@@ -12,12 +12,12 @@ const methodMahasiswa = {
         console.log(`energimu berkurang sebanyak ${aktifitas}`);
         console.log(`halo ${this.nama} selamat bermain!!`);
     },
-    
-    tidur: function(jam){
-        this.energi += jam *2;
-        console.log(`energimu bertambah sebanyak ${jam*2}`);
+
+    tidur: function (jam) {
+        this.energi += jam * 2;
+        console.log(`energimu bertambah sebanyak ${jam * 2}`);
         console.log(`halo ${this.nama} selamat tidur!!`);
-        
+
     }
 }
 
@@ -33,21 +33,21 @@ function Mahasiswa(nama, energi) {
 function MahasiswaConst(nama, energi) {
     this.nama = nama;
     this.energi = energi;
+}
 
-    this.makan = function (porsi) {
-        this.energi += porsi;
-        console.log(`jumlah makananmu ${jumlahMakan}`);
-        console.log(`total energimu ${energi}`);
+MahasiswaConst.prototype.makan = function (porsi) {
+    this.energi += porsi;
+    console.log(`halo ${this.nama} selamat makan!!`);
+}
 
-        console.log(`halo ${mahasiswa.nama}, selamat makan!!`);
-    }
+MahasiswaConst.prototype.bermain = function (main) {
+    this.energi -= main;
+    console.log(`halo ${this.nama} selamat bermain!!`);
+}
 
-    this.bermain = function (aktifitas) {
-        this.energi -= aktifitas;
-
-        console.log(`jumlah aktifitasmu ${aktifitas}`);
-        console.log(`halo ${this.nama}, selamat beraktifitas!!`);
-    }
+MahasiswaConst.prototype.tidur = function (jam) {
+    this.energi += jam * 2;
+    console.log(`halo ${this.nama} selamat tidur!!`);
 }
 
 let rama = new MahasiswaConst('rama', 10)
