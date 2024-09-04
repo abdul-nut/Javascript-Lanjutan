b9b297aec9fd8927d68e60556a9f33ca6683f808
abdul-nut <suryayasaantariksa@gmail.com>
2024-09-04 14:27:03 +0800

    versi classnya

--- Prototype/scripts.js
+++ Prototype/scripts.js
@@ -1,5 +1,4 @@
 // Function declaration
-
 const methodMahasiswa = {
     makan: function (porsi) {
         this.energi += porsi;
@@ -22,14 +21,13 @@ const methodMahasiswa = {
 }
 
 function Mahasiswa(nama, energi) {
-    let mahasiswa = Object.create(methosMahasiswa);
+    let mahasiswa = Object.create(methodMahasiswa);
     mahasiswa.nama = nama;
     mahasiswa.energi = energi;
     return mahasiswa;
 }
 
 // Constructor function
-
 function MahasiswaConst(nama, energi) {
     this.nama = nama;
     this.energi = energi;
@@ -49,6 +47,32 @@ MahasiswaConst.prototype.tidur = function (jam) {
     this.energi += jam * 2;
     console.log(`halo ${this.nama} selamat tidur!!`);
 }
+
+// versi clasnya
+
+class MahasiswaClass {
+    constructor(nama, energi) {
+        this.nama = nama;
+        this.energi = energi;
+    }
+
+    makan(porsi) {
+        this.energi += porsi;
+        console.log(`halo ${this.nama} selamat makan!!`);
+    }
+
+    main(main) {
+        this.energi -= main;
+        console.log(`halo ${this.nama} selamat bermain!!`);
+    }
+
+    tidur(jam) {
+        this.energi += jam * 2;
+        console.log(`halo ${this.nama} selamat tidur!!`);
+    }
+}
+
+let radit = new MahasiswaClass('radit', 10);
 
 let rama = new MahasiswaConst('rama', 10)
 
